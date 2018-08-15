005a5352191be0144fa51747f5af19aea4649624
Ryosuke IWANAGA <me@riywo.com>
2018-08-14 21:56:02 -0700

    Initial Commit

---
+++ PreloadedPersistentContainer/PreloadedPersistentContainer.h
@@ -0,0 +1,19 @@
+//
+//  PreloadedPersistentContainer.h
+//  PreloadedPersistentContainer
+//
+//  Created by Ryosuke Iwanaga on 2018-08-14.
+//  Copyright © 2018 Ryosuke Iwanaga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PreloadedPersistentContainer.
+FOUNDATION_EXPORT double PreloadedPersistentContainerVersionNumber;
+
+//! Project version string for PreloadedPersistentContainer.
+FOUNDATION_EXPORT const unsigned char PreloadedPersistentContainerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PreloadedPersistentContainer/PublicHeader.h>
+
+
