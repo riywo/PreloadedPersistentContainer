f64cfb058c93d29f01a92ab0afec001eee6d87a6
Ryosuke IWANAGA <me@riywo.com>
2018-08-14 22:02:52 -0700

    Add targets, make scheme shared

---
+++ PreloadedPersistentContainer iOS/PreloadedPersistentContainer_iOS.h
@@ -0,0 +1,19 @@
+//
+//  PreloadedPersistentContainer_iOS.h
+//  PreloadedPersistentContainer iOS
+//
+//  Created by Ryosuke Iwanaga on 2018-08-14.
+//  Copyright © 2018 Ryosuke Iwanaga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PreloadedPersistentContainer_iOS.
+FOUNDATION_EXPORT double PreloadedPersistentContainer_iOSVersionNumber;
+
+//! Project version string for PreloadedPersistentContainer_iOS.
+FOUNDATION_EXPORT const unsigned char PreloadedPersistentContainer_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PreloadedPersistentContainer_iOS/PublicHeader.h>
+
+
